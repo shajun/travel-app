debe25f824b00c01445b66dfbb4f324d583373bb
shajun <junco.sha@gmail.com>
2020-02-26 18:41:03 +0800

    feat: Set webpack and loaders plugins mode

---
+++ src/client/index.js
@@ -0,0 +1,15 @@
+// import { checkForURL } from './js/urlChecker';
+import { handleSubmit } from './js/app';
+// import { cutString } from './js/textControl';
+
+import './styles/resets.scss';
+// import './styles/base.scss';
+// import './styles/form.scss';
+// import './styles/footer.scss';
+// import './styles/header.scss';
+
+console.log('client running');
+
+// alert('I EXIST');
+
+export { handleSubmit };
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,1 +1,56 @@
-// init
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // check what text was put into the form field
+  // let inputText = document.getElementById('inputText').value;
+
+  // if (Client.checkForURL(inputText)) {
+  //   postData('http://localhost:8081/addData', {
+  //     formText: inputText
+  //   }).then(function(data) {
+  //     // update UI
+  //     document.getElementById('polarity').innerHTML = data.polarity;
+  //     document.getElementById('subjectivity').innerHTML = data.polarity;
+  //     document.getElementById('text').innerHTML = Client.cutString(
+  //       data.text,
+  //       500
+  //     );
+  //     document.getElementById('polarity_confidence').innerHTML =
+  //       data.polarity_confidence;
+  //     document.getElementById('subjectivity_confidence').innerHTML =
+  //       data.subjectivity_confidence;
+
+  //     // empty error message
+  //     document.getElementById('errorMessage').innerHTML = '';
+  //   });
+  // } else {
+  //   // error message
+  //   document.getElementById('errorMessage').innerHTML =
+  //     'Please enter the correct URL link address';
+  // }
+
+  console.log('::: Form Submitted :::');
+}
+
+// // TODO-Async POST
+// const postData = async (url = '', data = {}) => {
+//   const response = await fetch(url, {
+//     method: 'POST', // *GET, POST, DELETE, etc.
+//     credentials: 'same-origin', // include, *same-origin, omit
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data)
+//   });
+
+//   try {
+//     const newData = await response.json();
+//     console.log('postData', newData);
+//     return newData;
+//   } catch (error) {
+//     console.log('error', error);
+//     // appropriately handle the error
+//   }
+// };
+
+export { handleSubmit };
